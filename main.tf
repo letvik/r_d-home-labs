27ac6899d708b25734611654e6198e13b34b4f1e
Roman Lytvynenko <rletvik@gmail.com>
2025-06-29 17:33:42 +0200

    fix and destroy

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public_zone1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
